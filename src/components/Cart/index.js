b8c41bbcd7ba3813924a7ac97b34b79a4b865f90
tabishabbasidev <tabishabbasidev@gmail.com>
2024-02-09 08:53:39 +0530

    Add cart route and cart item component

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,3 +1,90 @@
+import "./index.css";
+import Header from "../Header";
+import { useContext } from "react";
+import { CartContext } from "../Context/CartContext";
+import CartItem from "../CartItem";
+import { Link } from "react-router-dom";
+
 export default function Cart() {
-  return <div>Cart</div>;
+  const { cartList } = useContext(CartContext);
+
+  const getTotalPriceOfCartItems = () => {
+    let totalPrice = 0;
+    for (let i = 0; i < cartList.length; i++) {
+      const price = parseFloat(cartList[i].price.slice(1));
+      const quantity = cartList[i].quantity;
+      totalPrice += price * quantity;
+    }
+
+    return Math.ceil(totalPrice);
+  };
+
+  const renderOrderSummary = () => {
+    return (
+      <li className="order-summary-container">
+        <p className="order-summary-para">Order Summary</p>
+        <div className="first-section">
+          <p className="amount-payable-para">
+            Amount Payable:
+            <br />
+            (Inclusive all taxes)
+          </p>
+          <p className="total-price">{getTotalPriceOfCartItems()}$/-</p>
+        </div>
+        <div className="second-section">
+          <p className="you-save">You Save</p>
+          <p className="saved-price">0$/-</p>
+        </div>
+        <p className="delievery-instruction-para">
+          Special Delievery Instructions
+        </p>
+        <textarea
+          placeholder="Please update special delievery instructions.."
+          className="text-area"
+        />
+        <Link to="/checkout">
+          <button title="click to checkout" className="checkout-btn">
+            Checkout
+          </button>
+        </Link>
+      </li>
+    );
+  };
+
+  const renderEmptyCartView = () => {
+    return (
+      <div className="empty-cart-view-container">
+        <h1>Your Cart Is Empty</h1>
+      </div>
+    );
+  };
+
+  const renderNonEmptyCartView = () => {
+    return (
+      <div className="cart-item-and-summary-container">
+        <ul className="cart-container">
+          {cartList.map((eachItem) => (
+            <CartItem key={eachItem.id} eachItem={eachItem} />
+          ))}
+        </ul>
+        {renderOrderSummary()}
+      </div>
+    );
+  };
+
+  const renderCartOrEmptyCartView = () => {
+    switch (cartList.length) {
+      case 0:
+        return renderEmptyCartView();
+      default:
+        return renderNonEmptyCartView();
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      {renderCartOrEmptyCartView()}
+    </>
+  );
 }
---
+++ src/components/CartItem/index.js
@@ -0,0 +1,52 @@
+import "./index.css";
+import { useState, useContext } from "react";
+import { CartContext } from "../../components/Context/CartContext";
+
+export default function CartItem({ eachItem }) {
+  const { id, image, title, subtitle, price, quantity } = eachItem;
+  const { updateExistingItemQuantity, removeItemFromCartList } =
+    useContext(CartContext);
+  const [productQuantity, setProductQuantity] = useState(quantity);
+
+  const quantityOptionsList = [];
+  for (let i = 1; i <= 30; i++) {
+    quantityOptionsList.push(i);
+  }
+
+  const handleQuantitySelector = (e) => {
+    setProductQuantity(parseInt(e.target.value));
+    updateExistingItemQuantity(id, parseInt(e.target.value));
+  };
+
+  const handleClickRemoveItem = () => {
+    removeItemFromCartList(id);
+  };
+
+  return (
+    <li className="cart-item">
+      <div className="cart-item-img-container">
+        <img className="product-img" src={image} alt={title} />
+      </div>
+      <div className="product-details-container">
+        <p className="product-title">{title}</p>
+        <p className="product-subtitle">{subtitle}</p>
+        <p className="product-price">{price}</p>
+      </div>
+      <div className="cart-btns-container">
+        <button className="cart-remove-btn" onClick={handleClickRemoveItem}>
+          Remove
+        </button>
+        <select
+          value={productQuantity}
+          title="select quantity"
+          className="quantity-selector"
+          onChange={handleQuantitySelector}
+        >
+          {quantityOptionsList.map((eachOption) => (
+            <option key={eachOption}>{eachOption}</option>
+          ))}
+        </select>
+      </div>
+    </li>
+  );
+}
