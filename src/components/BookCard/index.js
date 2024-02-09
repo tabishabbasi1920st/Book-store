3aec420f622644cebc60048f6cb0e8ff71374bc3
tabishabbasidev <tabishabbasidev@gmail.com>
2024-02-09 09:31:18 +0530

    Add functionality to add item in cartList in books component

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -1,8 +1,24 @@
 import "./index.css";
+import { CartContext } from "../Context/CartContext";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 export default function BookCard({ bookData }) {
   const { title, subtitle, isbn13, price, image, url } = bookData;
+
+  const { addItemIntoCartList } = useContext(CartContext);
+
+  const handleCartBtnClick = () => {
+    const item = {
+      id: isbn13,
+      image,
+      title,
+      subtitle,
+      price,
+      quantity: 1,
+    };
+    addItemIntoCartList(item);
+  };
 
   return (
     <li className="book-card">
@@ -15,7 +31,9 @@ export default function BookCard({ bookData }) {
         <p className="price">Rs. {price}/-</p>
       </div>
       <div className="cart-btn-container">
-        <button className="add-to-cart-btn">Add to cart</button>
+        <button onClick={handleCartBtnClick} className="add-to-cart-btn">
+          Add to cart
+        </button>
       </div>
     </li>
   );
